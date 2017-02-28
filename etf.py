7daf5e499d47b05c1b09a25b5b699e53af3b4291
Daniel Morgan <themorgantown@gmail.com>
2017-02-28 11:44:04 -0500

    Add Mac Notifications

--- etf.py
+++ etf.py
@@ -1,6 +1,8 @@
 import urllib2
 import threading
- 
+import os
+# First: brew install terminal-notifier
+
 def checkETF():
     threading.Timer(90.0, checkETF).start()
     target_url = "https://www.sec.gov/rules/sro/batsbzx.htm"
@@ -9,7 +11,17 @@ def checkETF():
  
     if "Bitcoin" in eachWord:
         print "Found"
+        notify(title = 'TO THE MOON?',
+            subtitle = 'GET ON IT',
+            message  = 'ETF NEWS')
     else:
         print "Nothing"
- 
+
+def notify(title, subtitle, message):
+    t = '-title {!r}'.format(title)
+    s = '-subtitle {!r}'.format(subtitle)
+    m = '-message {!r}'.format(message)
+    os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
+
+# Calling the function
 checkETF()
